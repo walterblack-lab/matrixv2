8687db4e1b4c7b0c11ea8d7cf69ab9652590bedf
walterblack-lab <1akosbagan@gmail.com>
2026-02-10 22:11:06 +0100

    Update net.lua

--- modules/net.lua
+++ modules/net.lua
@@ -1,1 +1,10 @@
+local Net = {}
+local RS = game:GetService("ReplicatedStorage")
 
+Net.Remotes = {
+    Attack = RS.Modules.Net["RE/RegisterAttack"],
+    StartQuest = RS.Modules.Net["RF/StartSubclassQuest"],
+    QuestUpdate = RS.Remotes.QuestUpdate
+}
+
+return Net
